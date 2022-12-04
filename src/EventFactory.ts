79e7f3d2657a4ed70dafbb61f32568f8efa9e6cb
Nonit Mittal <nonitmittal@gmail.com>
2022-12-04 08:06:38 +0530

    - fixed some bugs

--- src/EventFactory.ts
+++ src/EventFactory.ts
@@ -25,22 +25,22 @@ export function handleEventCreated(event: EventCreated): void {
         }
         // create attendeesList
         let attendeesList: string[] = [];
-        let recipientList: string[] = [];
+        // let recipientList: string[] = [];
 
-        for (let i = 0; i < event.params.attendees.length; i++) {
-            let attendee = event.params.attendees[i];
-            let obj = new Attendee(
-                event.params.eventAddress.toHex() + attendee.toHex()
-            );
-            obj.address = attendee;
-            obj.status = "INVITED";
-            obj.rsvpResponse = "PENDING";
-            obj.save();
-            attendeesList.push(
-                event.params.eventAddress.toHex() + attendee.toHex()
-            );
-            recipientList.push(attendee.toHex());
-        }
+        // for (let i = 0; i < event.params.attendees.length; i++) {
+        //     let attendee = event.params.attendees[i];
+        //     let obj = new Attendee(
+        //         event.params.eventAddress.toHex() + attendee.toHex()
+        //     );
+        //     obj.address = attendee;
+        //     obj.status = "INVITED";
+        //     obj.rsvpResponse = "PENDING";
+        //     obj.save();
+        //     attendeesList.push(
+        //         event.params.eventAddress.toHex() + attendee.toHex()
+        //     );
+        //     recipientList.push(attendee.toHex());
+        // }
 
         // add creator to list
         let obj = new Attendee(
@@ -53,12 +53,12 @@ export function handleEventCreated(event: EventCreated): void {
         attendeesList.push(
             event.params.eventAddress.toHex() + event.params.creator.toHex()
         );
-        recipientList.push(event.params.creator.toHex());
+        let recipientList = event.params.creator.toHex();
         eve.attendees = attendeesList;
 
         // push notification to the attendees
         let recipient = recipientList,
-            type = "4",
+            type = "3",
             title = "New Event Created",
             body = `New Event created by ${event.params.creator.toHex()}`,
             subject = "Event created",
@@ -68,9 +68,9 @@ export function handleEventCreated(event: EventCreated): void {
             cta = "https://epns.io/";
 
         let notification = `{\"type\": \"${type}\", \"title\": \"${title}\", \"body\": \"${body}\", \"subject\": \"${subject}\", \"message\": \"${message}\", \"image\": \"${image}\", \"secret\": \"${secret}\", \"cta\": \"${cta}\"}`;
-        sendEPNSNotification(recipient.toString(), notification);
+        sendEPNSNotification(recipient, notification);
 
-        eve.recipientList = recipientList.toString();
+        eve.recipientList = recipientList;
         eve.save();
     }
 }
